351575bf8117fdd8ab8a0ad779a5319ef5deb3cf
MostafaNazmulHasan <mostafanazmulhasan@gmail.com>
2021-08-02 23:56:59 +0600

    edited

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Card, Form } from "react-bootstrap";
-import { Map } from "react-bootstrap-icons";
 import { useParams } from "react-router";
 import fakeData from "../FakeData/FakeData";
-import '../GoogleMap/GoogleMap'
+import '../Destination/Destination.css'
+// import '../GoogleMap/GoogleMap'
 const Destination = () => {
   const { name } = useParams();
   const found = fakeData.find((element) => element.name === name);
@@ -13,25 +13,29 @@ const Destination = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-4 text-left">
-          <Card>
+        <div className="col-md-4 text-left ">
+          <Card className="card">
             <Card.Body>
-              <span className="fromTo">Pick From</span>
+              <span className="fromTo"><b>Pick From</b></span>
+              <br />
               <Form.Group>
                 <Form.Control type="text" placeholder="Mirpur-1" required />
               </Form.Group>
-              <span className="fromTo">Pick TO</span>
+              <br />
+              <span className="fromTo"><b>Pick TO</b></span>
+              <br />
               <Form.Group>
                 <Form.Control type="text" placeholder="Dhanmondi" required />
               </Form.Group>
-              <button className="searchBtn">
+              <br />
+              <button className="searchBtn btn-primary btn-block text-center">
                 Search
               </button>
             </Card.Body>
             <img src={image} alt="" />
           </Card>
         </div>
-        <div className="col-md-8">{<Map></Map>}</div>
+        {/* <div className="col-md-8">{<Map></Map>}</div> */}
       </div>
     </div>
   );
--- src/Components/GoogleMap/GoogleMap.js
+++ src/Components/GoogleMap/GoogleMap.js
@@ -1,19 +1,19 @@
-import { GoogleApiWrapper, InfoWindow, Map, Marker } from "google-maps-react";
-import { Component } from "react";
+// import { GoogleApiWrapper, InfoWindow, Map, Marker } from "google-maps-react";
+// import { Component } from "react";
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <div className="fullMap">
-        <Map google={this.props.google} zoom={14}>
-          <Marker onClick={this.onMarkerClick} name={"Current location"} />
-          <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
-        </Map>
-      </div>
-    );
-  }
-}
+// export class MapContainer extends Component {
+//   render() {
+//     return (
+//       <div className="fullMap">
+//         <Map google={this.props.google} zoom={14}>
+//           <Marker onClick={this.onMarkerClick} name={"Current location"} />
+//           <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
+//         </Map>
+//       </div>
+//     );
+//   }
+// }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyDdQ8vziDaGnni-skaZLJKbQhUXNOwvR7I",
-})(MapContainer);
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyDdQ8vziDaGnni-skaZLJKbQhUXNOwvR7I",
+// })(MapContainer);
